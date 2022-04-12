cd47d1a833b775e33d3eb095c0eaba37666445e8
Adrivinci <sheerikie@gmail.com>
2022-04-12 14:22:57 +0300

    version 1

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import './App.css';
+import ListItems from './ListItems';
+import React from 'react';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
+
+
+
+library.add(faTrash);
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput=this.handleInput.bind(this);
+    this.addItem=this.addItem.bind(this);
+    this.deleteItem=this.deleteItem.bind(this);
+    this.setUpdate=this.setUpdate.bind(this);
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text:e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();
+    const newItem =this.state.currentItem;
+    console.log(newItem);
+    if(newItem.text !== ""){
+      const newItems=[...this.state.items,newItem];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+  deleteItem(key){
+    const filteredItems=this.state.items.filter(item=>item.key!==key);
+    this.setState({
+      items:filteredItems
+    })
+  }
+  setUpdate(key){
+    const filteredItems=this.state.items.filter(item=>item.key!==key);
+    this.setState({
+      items:filteredItems
+    })
+  }
+  render(){
+    return (
+      <div className='App'>
+      <header>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <h1 style={{textAlign: "center"}}>Cocos Island</h1>
+          <input type="text" placeholder='Add text'
+          onChange={this.handleInput} />
+          <button type='submit'>Add</button>
+        </form>
+        <ListItems items={this.state.items}  deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
+    </header>
+    </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/ListItems.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ListItems.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
+
+export default function ListItems(props) {
+    const items=props.items;
+    const ListItems=items.map((item)=>{
+        return <div className='list' key={item.key}>
+            <p>
+            <input type="text" id={item.key} value={item.text}/>
+            <span>
+                <FontAwesomeIcon className='faicons' icon='trash' onClick={()=>props.deleteItem(item.key)}/>
+            </span>
+            </p>
+        </div>
+    })
+  return (
+    <div>
+    <FlipMove duration={500} easing="ease-in-out ">
+    {ListItems}
+    </FlipMove>
+    </div>
+  )
+}
