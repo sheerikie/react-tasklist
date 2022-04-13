5ff834e8596b0f38aca2bd3f47df97ce74999832
Adrivinci <sheerikie@gmail.com>
2022-04-13 12:21:04 +0300

    update fix

--- src/ListItems.js
+++ src/ListItems.js
@@ -8,7 +8,8 @@ export default function ListItems(props) {
     const ListItems=items.map((item)=>{
         return <div className='list' key={item.key}>
             <p>
-            <input type="text" id={item.key} value={item.text}/>
+            <input type="text" id={item.key} defaultValue={item.text} onChange={(e)=>{
+             props.setUpdate(e.target.value,item.key)}}/>
             <span>
                 <FontAwesomeIcon className='faicons' icon='trash' onClick={()=>props.deleteItem(item.key)}/>
             </span>
